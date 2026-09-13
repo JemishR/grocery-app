f2cb65616e73ad9a16c4f1b2dc47288bf688c91d
Jemish Ruparel <83460626+JemishR@users.noreply.github.com>
2026-09-13 18:55:18 +0530

    Update app.php

--- config/app.php
+++ config/app.php
@@ -1,18 +1,16 @@
 <?php
 
-use Illuminate\Support\Facades\Env;
-
 return [
-    'name' => Env::get('APP_NAME', 'Laravel'),
-    'env' => Env::get('APP_ENV', 'production'),
-    'debug' => (bool) Env::get('APP_DEBUG', false),
-    'url' => Env::get('APP_URL', 'http://localhost'),
+    'name' => env('APP_NAME', 'Laravel'),
+    'env' => env('APP_ENV', 'production'),
+    'debug' => (bool) env('APP_DEBUG', false),
+    'url' => env('APP_URL', 'http://localhost'),
     'timezone' => 'Asia/Kolkata',
     'locale' => 'en',
     'fallback_locale' => 'en',
     'faker_locale' => 'en_US',
     'cipher' => 'AES-256-CBC',
-    'key' => Env::get('APP_KEY'),
-    'previous_keys' => array_filter(explode(',', Env::get('APP_PREVIOUS_KEYS', ''))),
+    'key' => env('APP_KEY'),
+    'previous_keys' => array_filter(explode(',', env('APP_PREVIOUS_KEYS', ''))),
     'maintenance' => ['driver' => 'file'],
 ];
